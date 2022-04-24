ddbbcad0e504335740b34302c30222a1ebcc59cf
Eli3336 <mihai_elisabeta_3@yahoo.com>
2022-04-24 14:08:39 +0200

    structure

---
+++ Client/src/view/StartViewController.java
@@ -0,0 +1,36 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+import java.awt.*;
+
+public class StartViewController
+{
+  @FXML private TextField usernameLogin;
+  @FXML private PasswordField passwordLogin;
+
+  @FXML private TextField name;
+  @FXML private TextField usernameSignup;
+  @FXML private TextField phoneNumber;
+  @FXML private PasswordField passwordSignup;
+
+  public void init()
+  {
+
+  }
+
+  public void reset()
+  {
+
+  }
+
+  @FXML private void loginPressed()
+  {
+
+  }
+  @FXML private void signupPressed()
+  {
+
+  }
+}
