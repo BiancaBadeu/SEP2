f624e3f6a5819122bd11007506c0814efe806c72
Eli3336 <mihai_elisabeta_3@yahoo.com>
2022-04-25 13:40:01 +0200

    start javadoc

--- Client/src/view/StartViewController.java
+++ Client/src/view/StartViewController.java
@@ -5,6 +5,9 @@ import javafx.scene.control.PasswordField;
 
 import java.awt.*;
 
+/**
+ * A class representing a controller for the start.fxml file @see view.start.fxml
+ */
 public class StartViewController
 {
   @FXML private TextField usernameLogin;
@@ -15,6 +18,18 @@ public class StartViewController
   @FXML private TextField phoneNumber;
   @FXML private PasswordField passwordSignup;
 
+  /**
+   * A 0 argument empty constructor
+   */
+  public StartViewController(){}
+
+  /**
+   * @param viewHandler a ViewHandler variable for control over the GUI
+   * @param viewmodel a
+   * @param root a
+   *
+   * A method to initiate the GUI windows and initializing the parameters previously mentioned
+   */
   public void init()
   {
 
@@ -25,6 +40,9 @@ public class StartViewController
 
   }
 
+  /**
+   * A FXML method called when the button named Log in is pressed. what is does
+   */
   @FXML private void loginPressed()
   {
 
