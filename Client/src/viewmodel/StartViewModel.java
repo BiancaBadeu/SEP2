0ad9d4207efaafa21226839455a3dda745595260
BiancaBadeu <badeubianca@gmail.com>
2022-04-27 10:48:28 +0200

    viewmodel updates

--- Client/src/viewmodel/StartViewModel.java
+++ Client/src/viewmodel/StartViewModel.java
@@ -4,6 +4,10 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import model.Model;
 
+/**
+ A viewModel class that represents the functionality of the Controller class StartViewController
+ * @see view.StartViewController
+ **/
 public class StartViewModel
 {
   private Model model;
@@ -12,11 +16,16 @@ public class StartViewModel
   private StringProperty errorLabelLogin;
   private StringProperty name;
   private StringProperty usernameSignup;
-  private StringProperty errorUsername;
   private StringProperty phoneNumber;
   private StringProperty passwordSignup;
-  private StringProperty errorDob;
+  private StringProperty errorSignup;
   private StringProperty dateLabel;
+
+  /**
+   * @param model a Model variable so that the viewModel has access to the model's methods
+   *
+   * An 1 argument constructor that initializes the instance variables
+   */
 
   public StartViewModel(Model model)
   {
@@ -26,12 +35,15 @@ public class StartViewModel
     this.errorLabelLogin = new SimpleStringProperty();
     this.name = new SimpleStringProperty();
     this.usernameSignup = new SimpleStringProperty();
-    this.errorUsername = new SimpleStringProperty();
     this.phoneNumber = new SimpleStringProperty();
     this.passwordSignup = new SimpleStringProperty();
-    this.errorDob = new SimpleStringProperty();
+    this.errorSignup = new SimpleStringProperty();
     this.dateLabel = new SimpleStringProperty();
   }
+
+  /**
+   * A method to reset the instance variables everytime we open the window
+   */
 
   public void reset()
   {
@@ -40,57 +52,143 @@ public class StartViewModel
     errorLabelLogin.set("");
     name.set("");
     usernameSignup.set("");
-    errorUsername.set("");
     phoneNumber.set("");
     passwordSignup.set("");
-    errorDob.set("");
+    errorSignup.set("");
     dateLabel.set("");
   }
+
+  /**
+   * @return the usernameLogin property
+   *
+   * A getter for the usernameLogin string property
+   */
 
   public  StringProperty getUsernameLoginProperty()
   {
     return usernameLogin;
   }
 
+  /**
+   * @return the passwordLogin property
+   *
+   * A getter for the passwordLogin string property
+   */
+
   public StringProperty getPasswordLoginProperty()
   {
     return passwordLogin;
   }
+
+  /**
+   * @return the errorLabelLogin property
+   *
+   * A getter for the errorLabelLogin string property
+   */
 
   public StringProperty getErrorLabelLoginProperty()
   {
     return errorLabelLogin;
   }
 
+  /**
+   * @return the name property
+   *
+   * A getter for the name string property
+   */
+
   public StringProperty getNameProperty()
   {
     return name;
   }
+
+  /**
+   * @return the usernameSignup property
+   *
+   * A getter for the usernameSignup string property
+   */
 
   public StringProperty getUsernameSignupProperty()
   {
     return usernameSignup;
   }
 
-  public StringProperty getErrorUsernameProperty()
-  {
-    return errorUsername;
-  }
+
+  /**
+   * @return the phoneNumber property
+   *
+   * A getter for the phoneNumber string property
+   */
 
   public StringProperty getPhoneProperty()
   {
     return phoneNumber;
   }
 
+  /**
+   * @return the passwordSignup property
+   *
+   * A getter for the passwordSignup string property
+   */
+
   public StringProperty getPasswordSignupProperty()
   {
     return passwordSignup;
   }
 
-  public StringProperty getErrorDobProperty()
+  /**
+   * @return the errorSignup property
+   *
+   * A getter for the errorSignup string property
+   */
+
+  public StringProperty getErrorSignupProperty()
   {
-    return errorDob;
+    return errorSignup;
   }
 
+  /**
+   * @return the dateLabel property
+   *
+   * A getter for the dateLabel string property
+   */
+
   public StringProperty getDateLabelProperty(){return dateLabel;}
+
+  /**
+   @return a boolean value that is true if the model doesn't throw an exception and false if this method catches an exception
+   @see model.Model
+   **/
+
+  public boolean login()
+  {
+    try{
+      model.login(usernameLogin.get(), passwordLogin.get());
+      return true;
+    }
+    catch(Exception e)
+    {
+      errorLabelLogin.set(e.getMessage());
+      return false;
+    }
+  }
+
+  /**
+   * @return a boolean value that is true if the model doesn't throw an exception and false if this method catches an exception
+   * @see model.Model
+   */
+  public boolean createUser()
+  {
+    try{
+      model.createUser(name.get(), usernameSignup.get(), passwordSignup.get(), phoneNumber.get(), dateLabel.get());
+      return true;
+    }
+    catch(Exception e)
+    {
+      errorSignup.set(e.getMessage());
+      return false;
+    }
+  }
+
+
 }
