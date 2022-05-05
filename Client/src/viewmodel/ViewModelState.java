744d5644587bb0f14f029ef47e5de53b319b02b6
Xbey25 <luisdgb2506@gmail.com>
2022-05-05 12:26:51 +0200

    State

--- Client/src/viewmodel/ViewModelState.java
+++ Client/src/viewmodel/ViewModelState.java
@@ -1,6 +1,23 @@
 package viewmodel;
 
+import model.Movie;
+
 public class ViewModelState
 {
 
+ private Movie movie;
+
+
+ public ViewModelState(Movie movie){
+   this.movie = movie;
+ }
+
+ public Movie getMovie(){
+   return movie;
+ }
+
+ public void setMovie(Movie movie){
+   this.movie = movie;
+ }
+ 
 }
