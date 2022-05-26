bb7d5c6c51c17e35add78799eaf257350a8316a0
Xbey25 <luisdgb2506@gmail.com>
2022-05-26 13:46:56 +0200

    fix stash issues

--- Client/Tests/RentalTest.java
+++ Client/Tests/RentalTest.java
@@ -1,16 +1,16 @@
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 //import static org.junit.jupiter.api.Assertions.*;
 
 class RentalTest
 {
 
- // @BeforeEach void setUp()
+  @BeforeEach void setUp()
   {
   }
 
- // @AfterEach void tearDown()
+  @AfterEach void tearDown()
   {
   }
 }
--- Client/Tests/UserTest.java
+++ Client/Tests/UserTest.java
@@ -1,16 +1,54 @@
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
+import model.domain.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest
 {
 
-  //@BeforeEach void setUp()
+  private User user1;
+
+  @BeforeEach void setUp()
+  {
+
+    user1 = new User("Test", "Test", "Test", "Test", 1);
+  }
+
+  @AfterEach void tearDown()
   {
   }
 
-  //@AfterEach void tearDown()
+  @Test void setInvalidPhoneNumber()
   {
+    assertThrows(IllegalArgumentException.class, () -> {
+      user1.setPhoneNumber("00000000");
+    });
   }
+
+  @Test void setInvalidName()
+  {
+    assertThrows(IllegalArgumentException.class, () -> {
+      user1.setName("1234");
+    });
+
+  }
+
+  @Test void setInvalidUserName()
+  {
+    assertThrows(IllegalArgumentException.class, () -> {
+      user1.setUserName("&*");
+    });
+  }
+
+  @Test void setNullUserName()
+  {
+    assertThrows(IllegalArgumentException.class, () -> {
+      user1.setUserName(null);
+    });
+
+  }
+
 }
+
