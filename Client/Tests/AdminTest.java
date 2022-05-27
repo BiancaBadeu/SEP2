f549c68b312db83c20fe716dc647377dc826e1eb
Xbey25 <luisdgb2506@gmail.com>
2022-05-27 10:13:31 +0200

    admin test

--- Client/Tests/AdminTest.java
+++ Client/Tests/AdminTest.java
@@ -1,16 +1,41 @@
+import model.domain.Admin;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdminTest
 {
 
+ Admin admin= new Admin("admin","admin","admin","admin",0);
  @BeforeEach void setUp()
   {
   }
 
   @AfterEach void tearDown()
   {
+  }
+
+
+  @Test void setInvalidName()
+  {
+    assertThrows(IllegalArgumentException.class, () -> {
+      admin.setName("");
+    });
+  }
+
+  @Test void setInvalidUsername()
+  {
+    assertThrows(IllegalArgumentException.class, () -> {
+      admin.setUserName("");
+    });
+  }
+
+  @Test void setInvalidPhoneNumber()
+  {
+    assertThrows(IllegalArgumentException.class, () -> {
+      admin.setPhoneNumber("");
+    });
   }
 }
