9bffe535a98d27e5a1e5a92b47668e92af764df2
domignat <92146634+domignat@users.noreply.github.com>
2022-05-29 17:12:49 +0200

    age test

--- Server/src/model/domain/Person.java
+++ Server/src/model/domain/Person.java
@@ -141,14 +141,12 @@ public abstract class Person implements Serializable
    */
   public void setPassword(String password)
   {
-
     if(password==null || password.equals(""))
     {
       throw new IllegalArgumentException("Invalid password");
     }
     else
       this.password = password;
-
   }
 
   /**
@@ -157,6 +155,11 @@ public abstract class Person implements Serializable
    */
   public void setAge(int age)
   {
+    if(age<0  || age>150)
+    {
+      throw new IllegalArgumentException("Invalid age");
+    }
+    else
     this.age = age;
   }
 
